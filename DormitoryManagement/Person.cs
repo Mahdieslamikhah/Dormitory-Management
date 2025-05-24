b02ed12e58f0a51b454f416fdb75774a4937e2d1
SalehKafi <salehkey52@gmail.com>
2025-05-24 21:43:59 +0330

    ..
    class person

--- DormitoryManagement/Person.cs
+++ DormitoryManagement/Person.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+class person
+{
+    public string Name { get; set; }
+    public string Lastname { get; set; }
+    public long PersonalID { get; set; }
+    public string Phonenumber { get; set; }
+    public string Adress { get; set; }
+    public person(string name, string lastname, long personalID, string phonenumber, string adress)
+    {
+        Name = name;
+        Lastname = lastname;
+        PersonalID = personalID;
+        Phonenumber = phonenumber;
+        Adress = adress;
+    }
+}
