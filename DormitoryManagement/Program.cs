08e5f7ab8cf38d6becf7e999d3d5404d76ffda8f
mAhdi Eslamikhah <97355553+Mahdieslamikhah@users.noreply.github.com>
2025-05-24 11:32:13 -0700

    Add welcome

--- DormitoryManagement/Program.cs
+++ DormitoryManagement/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace DormitoryManagement
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("______________________________");
+            Console.WriteLine();
+            Console.WriteLine("Welcom To Dormitory Management..");
+            Console.WriteLine("______________________________");
+            Console.WriteLine();
+        }
+    }
+}
