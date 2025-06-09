10673f3570ba4dcd2c06cb3948eb8cfac40faee5
SalehKafi <salehkey52@gmail.com>
2025-06-09 12:49:28 +0330

    ,

--- DormitoryManagement/obj/Debug/net9.0/DormitoryManagement.AssemblyInfo.cs
+++ DormitoryManagement/obj/Debug/net9.0/DormitoryManagement.AssemblyInfo.cs
@@ -15,7 +15,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5df47719c43fff2fcb4ebc1d8e81ef20869279b7")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2376d3ecbb8363412164b27427cd52cd1511c113")]
+>>>>>>> Stashed changes
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2376d3ecbb8363412164b27427cd52cd1511c113")]
 >>>>>>> Stashed changes
