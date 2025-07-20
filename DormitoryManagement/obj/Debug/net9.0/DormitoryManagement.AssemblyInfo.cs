4773544c50a4a658f202bffb56900124e7e12e1b
mAhdi Eslamikhah <97355553+Mahdieslamikhah@users.noreply.github.com>
2025-07-20 08:22:55 -0700

    peya kardan bug

--- DormitoryManagement/obj/Debug/net9.0/DormitoryManagement.AssemblyInfo.cs
+++ DormitoryManagement/obj/Debug/net9.0/DormitoryManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DormitoryManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb2d0ce2a1f13d4c4e86a38488a9719e42d5596b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+695309ba2947dbb492b5cf21d7f43b75b86abb05")]
 [assembly: System.Reflection.AssemblyProductAttribute("DormitoryManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DormitoryManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
